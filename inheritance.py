4e805ce937fc4dd8b1d8355ffb77ce58614877ea
sschneeberg <simone.m.schneeberg@gmail.com>
2021-01-08 12:54:03 -0500

    add iphone

--- inheritance.py
+++ inheritance.py
@@ -25,6 +25,22 @@ class Android(Phone):
     def get_my_number(self):
         print(self.number)
 
+class IPhone( Android):
+    def __init__(self, number, color):
+        super().__init__(number, color)
+        self.face_id = None
+
+    def __str__(self):
+        return f"this is an iPhone at {self.number}"
+
+    def set_face_id(self):
+        self.face_id = True
+
+
 android_phone = Android('123-456-7890', 'red')
 android_phone.get_my_number()
 android_phone.call('098-765-4321')
+
+iphone_phone = Android('123-456-7890', 'rose gold')
+iphone_phone.get_my_number()
+iphone_phone.call('098-765-4321')
